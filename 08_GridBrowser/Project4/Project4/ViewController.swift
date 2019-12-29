f4b266d7be39ca64755111feb6258519be017a5f
IKEH <pondau1024@gmail.com>
2019-12-30 08:37:58 +0900

    part2

--- 08_GridBrowser/Project4/Project4/ViewController.swift
+++ 08_GridBrowser/Project4/Project4/ViewController.swift
@@ -7,13 +7,34 @@
 //
 
 import Cocoa
+import WebKit
 
-class ViewController: NSViewController {
+class ViewController: NSViewController, WKNavigationDelegate {
+    
+    var rows: NSStackView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // Do any additional setup after loading the view.
+        // 1: create the stackView and add it to our view
+        rows = NSStackView()
+        rows.orientation = .vertical // デフォルトは.horizontal
+        rows.distribution = .fillEqually    // 等間隔
+        rows.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(rows)
+        
+        // 2: Create Auto Layout constraints that pin the stack view to the edges of its container
+        rows.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        rows.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+        rows.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        rows.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+        
+        // 3: Create an initial column that contains a single web view
+        let column = NSStackView(views: [makeWebView()])
+        column.distribution = .fillEqually
+        
+        // 4: Add this column to the `rows` stack view
+        rows.addArrangedSubview(column)
     }
     
     override var representedObject: Any? {
@@ -36,6 +57,16 @@ class ViewController: NSViewController {
     
     @IBAction func adjustColumns(_ sender: NSSegmentedControl) {
         
+    }
+    
+    func makeWebView() -> NSView {
+        
+        let webView = WKWebView()
+        webView.navigationDelegate = self
+        webView.wantsLayer = true
+        webView.load(URLRequest(url: URL(string: "https://www.apple.com")!))
+        
+        return webView
     }
 }
 
---
+++ 08_GridBrowser/memo01.md
@@ -0,0 +1,25 @@
+- `rows.translatesAutoresizingMaskIntoConstraints = false`
+    - AutoLayoutと設定がバッティングしてしまうのでOFFにする
+
+>[Auto Layoutをコードから使おう](https://blog.personal-factory.com/2016/01/11/make-auto-layout-via-code/)
+> 生成したビューのプロパティにtranslatesAutoresizingMaskIntoConstraintsというものがあります。
+> これをオフにします。
+> このプロパティはAuto Layout以前に使われていた、Autosizingのレイアウトの仕組みをAuto Layoutに変換するかどうかを設定するフラグです。
+> デフォルトではオンになっていて、オンのままだと期待通りのAuto Layout設定ができない場合があるのでオフにします。    
+
+![image](https://i.imgur.com/uHqYe8F.png)
+
+- `wantsLayer`はiOSと違ってデフォルトでOFFになっている
+- ACTIVE状態を示す青枠を描画するために`YES`にしているとか
+
+![image](https://i.imgur.com/vUJsskI.png)
+
+![image](https://i.imgur.com/N7I4Orn.png)
+
+- コードによるAutoLayout制約は下記が分かりやすかった。
+    - [\[Swift 3\.0\]NSLayoutAnchorを用いたコードによるAuto Layout \- Qiita](https://qiita.com/shindooo/items/36d2e8bf9d8ba3fa4ed5)
+
+```swift
+rows.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+```
+
