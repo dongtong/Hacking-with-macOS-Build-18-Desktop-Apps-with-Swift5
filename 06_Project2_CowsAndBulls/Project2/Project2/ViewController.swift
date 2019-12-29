2502e07ec4c8136c4fb2869f5d19a76f15083d09
IKEH <pondau1024@gmail.com>
2019-12-29 18:52:35 +0900

    part2

--- 06_Project2_CowsAndBulls/Project2/Project2/ViewController.swift
+++ 06_Project2_CowsAndBulls/Project2/Project2/ViewController.swift
@@ -8,11 +8,13 @@
 
 import Cocoa
 
-class ViewController: NSViewController {
+class ViewController: NSViewController, NSTableViewDataSource, NSTableViewDelegate {
 
+    var answer = ""
+    var guesses = [String]()
+    
     @IBOutlet var tableView: NSTableView!
     @IBOutlet var guess: NSTextField!
-    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -26,8 +28,35 @@ class ViewController: NSViewController {
         }
     }
 
-    @IBAction func submitGuess(_ sender: Any) {
+    func numberOfRows(in tableView: NSTableView) -> Int {
+        return guesses.count
     }
     
+    @IBAction func submitGuess(_ sender: Any) {
+        
+    }
+    
+    func result(for guess: String) -> String {
+        
+        return "Result" // e.g. 1c1b
+    }
+    
+    func tableView(_ tableView: NSTableView, viewFor tableColumn: NSTableColumn?, row: Int) -> NSView? {
+        guard let vw = tableView.makeView(withIdentifier: tableColumn!.identifier, owner: self) as? NSTableCellView else { return nil }
+        
+        if tableColumn?.title == "Guess" {
+            
+            // this is the "Guess" column; show a previous guess
+            vw.textField?.stringValue = result(for: guesses[row])
+        }
+        
+        return vw
+    }
+    
+    func tableView(_ tableView: NSTableView, shouldSelectRow row: Int) -> Bool {
+        
+        return false
+    }
+
 }
 
