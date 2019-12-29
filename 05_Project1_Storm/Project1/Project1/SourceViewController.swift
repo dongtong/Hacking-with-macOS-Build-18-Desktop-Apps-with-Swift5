ae0d2de042e1ac1abaf3e814f64163923861bb66
IKEH <pondau1024@gmail.com>
2019-12-29 12:17:53 +0900

    lecture11

--- 05_Project1_Storm/Project1/Project1/SourceViewController.swift
+++ 05_Project1_Storm/Project1/Project1/SourceViewController.swift
@@ -8,13 +8,28 @@
 
 import Cocoa
 
-class SourceViewController: NSViewController {
+class SourceViewController: NSViewController, NSTableViewDataSource, NSTableViewDelegate {
 
   @IBOutlet var tableView: NSTableView!
   
   override func viewDidLoad() {
         super.viewDidLoad()
         // Do view setup here.
+  }
+  
+  func numberOfRows(in tableView: NSTableView) -> Int {
+    return 100
+  }
+  
+  func tableView(_ tableView: NSTableView,
+                 viewFor tableColumn: NSTableColumn?,
+                 row: Int) -> NSView? {
+    guard let vw = tableView.makeView(withIdentifier: tableColumn!.identifier, owner: self) as? NSTableCellView else {
+      return nil
     }
+    vw.textField?.stringValue = "Hello, world!"
     
+    return vw
+  }
 }
+
