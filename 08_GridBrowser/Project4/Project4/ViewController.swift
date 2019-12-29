bdd51104a3f293a9e80cae5cb03d474097ddd156
IKEH <pondau1024@gmail.com>
2019-12-30 00:15:02 +0900

    part1_01

--- 08_GridBrowser/Project4/Project4/ViewController.swift
+++ 08_GridBrowser/Project4/Project4/ViewController.swift
@@ -9,19 +9,33 @@
 import Cocoa
 
 class ViewController: NSViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
     }
-
+    
     override var representedObject: Any? {
         didSet {
-        // Update the view, if already loaded.
+            // Update the view, if already loaded.
         }
     }
-
-
+    
+    @IBAction func urlEntered(_ sender: NSTextField) {
+        
+    }
+    
+    @IBAction func navigationClicked(_ sender: NSSegmentedControl) {
+        
+    }
+    
+    @IBAction func adjustRows(_ sender: NSSegmentedControl) {
+        
+    }
+    
+    @IBAction func adjustColumns(_ sender: NSSegmentedControl) {
+        
+    }
 }
 
