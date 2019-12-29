8e54dcc84e8ae0ca689b27c3841b502d217a9d6d
IKEH <pondau1024@gmail.com>
2019-12-29 14:14:09 +0900

    lecture14

--- 05_Project1_Storm/Project1/Project1/AppDelegate.swift
+++ 05_Project1_Storm/Project1/Project1/AppDelegate.swift
@@ -10,17 +10,22 @@ import Cocoa
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
-
-
-
-  func applicationDidFinishLaunching(_ aNotification: Notification) {
-    // Insert code here to initialize your application
-  }
-
-  func applicationWillTerminate(_ aNotification: Notification) {
-    // Insert code here to tear down your application
-  }
-
-
+    
+    
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+    
+    // ウィンドウを削除するときにアプリケーションを終了する
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+    
+    
 }
 
