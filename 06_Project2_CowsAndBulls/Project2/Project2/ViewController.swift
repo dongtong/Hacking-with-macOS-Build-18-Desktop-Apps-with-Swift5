5086b23f442e3351a823d705673a477d8ff12f71
IKEH <pondau1024@gmail.com>
2019-12-29 17:55:29 +0900

    create project

---
+++ 06_Project2_CowsAndBulls/Project2/Project2/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Project2
+//
+//  Created by Hiroki Ikeuchi on 2019/12/29.
+//  Copyright © 2019 ikeh1024. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
