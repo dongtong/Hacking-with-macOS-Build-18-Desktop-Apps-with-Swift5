48bd6e6e92537030d478bd9952061d8cf620fbc8
IKEH <pondau1024@gmail.com>
2019-12-29 20:31:02 +0900

    part1

--- 97_SocialMedia/Project1/Project1/ViewController.swift
+++ 97_SocialMedia/Project1/Project1/ViewController.swift
@@ -8,20 +8,30 @@
 
 import Cocoa
 
-class ViewController: NSViewController {
-
-  override func viewDidLoad() {
-    super.viewDidLoad()
-
-    // Do any additional setup after loading the view.
-  }
-
-  override var representedObject: Any? {
-    didSet {
-    // Update the view, if already loaded.
+class ViewController: NSSplitViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
     }
-  }
-
-
+    
+    override var representedObject: Any? {
+        didSet {
+            // Update the view, if already loaded.
+        }
+    }
+    
+    @IBAction func sharedClicked(_ sender: NSView) {
+        
+        guard let detail = children[1] as? DetailViewController else { return }
+        guard let image = detail.imageView.image else { return }
+        
+        let picker = NSSharingServicePicker(items: [image])
+        
+        picker.show(relativeTo: .zero, of: sender, preferredEdge: .minY)
+    }
+    
+    
 }
 
