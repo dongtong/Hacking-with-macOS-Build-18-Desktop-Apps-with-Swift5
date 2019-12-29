46d6faa9f8592d779fc5025c6760d22c0d331204
IKEH <pondau1024@gmail.com>
2019-12-29 12:57:41 +0900

    lecture12

--- 05_Project1_Storm/Project1/Project1/SourceViewController.swift
+++ 05_Project1_Storm/Project1/Project1/SourceViewController.swift
@@ -9,27 +9,42 @@
 import Cocoa
 
 class SourceViewController: NSViewController, NSTableViewDataSource, NSTableViewDelegate {
-
-  @IBOutlet var tableView: NSTableView!
-  
-  override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do view setup here.
-  }
-  
-  func numberOfRows(in tableView: NSTableView) -> Int {
-    return 100
-  }
-  
-  func tableView(_ tableView: NSTableView,
-                 viewFor tableColumn: NSTableColumn?,
-                 row: Int) -> NSView? {
-    guard let vw = tableView.makeView(withIdentifier: tableColumn!.identifier, owner: self) as? NSTableCellView else {
-      return nil
-    }
-    vw.textField?.stringValue = "Hello, world!"
     
-    return vw
-  }
+    @IBOutlet var tableView: NSTableView!
+    var pictures = [String]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // get a list of all the files in our bundle's resource paht
+        let fm = FileManager.default
+        let path = Bundle.main.resourcePath!
+        let items = try! fm.contentsOfDirectory(atPath: path)
+        
+        // loop over each file we found
+        for item in items {
+            
+            // if this item starts with "nssl" add it to our array
+            // nssl:National Severe Stroms Laboratory
+            if item.hasPrefix("nssl") {
+                pictures.append(item)
+            }
+        }
+    }
+    
+    func numberOfRows(in tableView: NSTableView) -> Int {
+        return pictures.count
+    }
+    
+    func tableView(_ tableView: NSTableView,
+                   viewFor tableColumn: NSTableColumn?,
+                   row: Int) -> NSView? {
+        guard let vw = tableView.makeView(withIdentifier: tableColumn!.identifier, owner: self) as? NSTableCellView else {
+            return nil
+        }
+        vw.textField?.stringValue = pictures[row]
+        
+        return vw
+    }
 }
 
