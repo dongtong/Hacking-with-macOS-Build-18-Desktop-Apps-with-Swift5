25d99d615d7b8b0b8f3a20e6940ee9cc32d7a48f
IKEH <pondau1024@gmail.com>
2019-12-29 11:30:03 +0900

    5-7

---
+++ 05_Project1_Storm/Project1/Project1/DetailViewController.swift
@@ -0,0 +1,18 @@
+//
+//  DetailViewController.swift
+//  Project1
+//
+//  Created by Hiroki Ikeuchi on 2019/12/29.
+//  Copyright © 2019 ikeh1024. All rights reserved.
+//
+
+import Cocoa
+
+class DetailViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
