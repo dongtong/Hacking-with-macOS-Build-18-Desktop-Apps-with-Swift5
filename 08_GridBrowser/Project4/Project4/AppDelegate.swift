0c84c5c70de48254b869f1bb1336d1b90a7992eb
IKEH <pondau1024@gmail.com>
2019-12-29 23:33:11 +0900

    create project

---
+++ 08_GridBrowser/Project4/Project4/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Project4
+//
+//  Created by Hiroki Ikeuchi on 2019/12/29.
+//  Copyright © 2019 ikeh1024. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
