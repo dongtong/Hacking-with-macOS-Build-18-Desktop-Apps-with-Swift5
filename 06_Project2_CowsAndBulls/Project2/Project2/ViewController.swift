0ddb589ea252938d6d59e1b318b5b1f70ced20c5
IKEH <pondau1024@gmail.com>
2019-12-29 19:59:18 +0900

    part4

--- 06_Project2_CowsAndBulls/Project2/Project2/ViewController.swift
+++ 06_Project2_CowsAndBulls/Project2/Project2/ViewController.swift
@@ -34,12 +34,52 @@ class ViewController: NSViewController, NSTableViewDataSource, NSTableViewDelega
     }
     
     @IBAction func submitGuess(_ sender: Any) {
+
+        // check for 4 unique characters
+        let guessString = guess.stringValue
+        guard Set(guessString).count == 4 else { return }
         
+        // ensure there are no non-digit characters
+        let badCharacters = CharacterSet(charactersIn: "0123456789").inverted
+        guard guessString.rangeOfCharacter(from: badCharacters) == nil else { return }
+        
+        // add the guess to the array and table view
+        guesses.insert(guessString, at: 0)
+        tableView.insertRows(at: IndexSet(integer: 0), withAnimation: .slideDown)
+        
+        // did the player win?
+        let resultString = result(for: guessString)
+        
+        if resultString.contains("4b") {
+            let alert = NSAlert()
+            alert.messageText = "You win!"
+            alert.informativeText = "Congratulations! Click OK to play again."
+            
+            alert.runModal()
+            
+            startNewGame()
+        }
     }
     
     func result(for guess: String) -> String {
+        var bulls = 0
+        var cows = 0
         
-        return "Result" // e.g. 1c1b
+        let guessLetters = Array(guess)
+        let answerLetters = Array(answer)
+        
+        for (index, letter) in guessLetters.enumerated() {
+            
+            if letter == answerLetters[index] {
+                
+                bulls += 1
+            } else if answerLetters.contains(letter) {
+                
+                cows += 1
+            }
+        }
+        
+        return "\(bulls)b \(cows)c"
     }
     
     func tableView(_ tableView: NSTableView, viewFor tableColumn: NSTableColumn?, row: Int) -> NSView? {
@@ -48,6 +88,10 @@ class ViewController: NSViewController, NSTableViewDataSource, NSTableViewDelega
         if tableColumn?.title == "Guess" {
             
             // this is the "Guess" column; show a previous guess
+            vw.textField?.stringValue = result(for: guesses[row])
+        } else {
+            
+            // this is the "Result" column; call our new method
             vw.textField?.stringValue = result(for: guesses[row])
         }
         
@@ -72,7 +116,7 @@ class ViewController: NSViewController, NSTableViewDataSource, NSTableViewDelega
             
             answer.append(String(numbers.removeLast()))
         }
-        
+//        print(answer)
         tableView.reloadData()
     }
     
