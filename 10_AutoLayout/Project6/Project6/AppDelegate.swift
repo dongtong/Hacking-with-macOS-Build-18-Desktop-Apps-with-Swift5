056ccbc7e35d8203a6d128f3cd652bc0cfe6fa87
IKEH <pondau1024@gmail.com>
2020-01-04 22:43:47 +0900

    create project

---
+++ 10_AutoLayout/Project6/Project6/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Project6
+//
+//  Created by Hiroki Ikeuchi on 2020/01/04.
+//  Copyright © 2020 ikeh1024. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
