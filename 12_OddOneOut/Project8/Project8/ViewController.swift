0be80da2230372981886b82c947d863782b3c612
IKEH <pondau1024@gmail.com>
2020-01-09 21:58:24 +0900

    part1_02

--- 12_OddOneOut/Project8/Project8/ViewController.swift
+++ 12_OddOneOut/Project8/Project8/ViewController.swift
@@ -45,6 +45,68 @@ class ViewController: NSViewController {
         visualEffectView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
         visualEffectView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
         visualEffectView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+        
+        let title = createTitle()
+        createGridView(relativeTo: title)
+    }
+    
+    func createTitle() -> NSTextField {
+        let titleString = "Odd OneOut"
+        let title = NSTextField(labelWithString: titleString)
+        title.font = NSFont.systemFont(ofSize: 36, weight: .thin)
+        title.textColor = NSColor.white
+        title.translatesAutoresizingMaskIntoConstraints = false
+        
+        visualEffectView.addSubview(title)
+        title.topAnchor.constraint(equalTo: visualEffectView.topAnchor,
+                                   constant: gridMargin).isActive = true
+        title.centerXAnchor.constraint(equalTo: visualEffectView.centerXAnchor).isActive = true
+        
+        return title
+    }
+    
+    func createButtonArray() -> [[NSButton]] {
+        var rows = [[NSButton]]()
+        
+        for _ in 0 ..< gridSize {
+            var row = [NSButton]()
+            for _ in 0 ..< gridSize {
+                let button = NSButton(frame: NSRect(x: 0, y: 0, width: 64, height: 64))
+                
+                button.image = NSImage(named: "penguin")
+                button.setButtonType(.momentaryChange)
+                button.imagePosition = .imageOnly
+                button.focusRingType = .none
+                button.isBordered = false
+                // button.action = #selector(iamgeClicked)
+                
+                gridViewButtons.append(button)
+                row.append(button)
+            }
+            rows.append(row)
+        }
+        return rows
+    }
+    
+    func createGridView(relativeTo title: NSTextField) {
+        let rows = createButtonArray()
+        let gridView = NSGridView(views: rows)
+        
+        gridView.translatesAutoresizingMaskIntoConstraints = false
+        visualEffectView.addSubview(gridView)
+        
+        gridView.leadingAnchor.constraint(equalTo: visualEffectView.leadingAnchor, constant: gridMargin).isActive = true
+        gridView.trailingAnchor.constraint(equalTo: visualEffectView.trailingAnchor, constant: -gridMargin).isActive = true
+        gridView.topAnchor.constraint(equalTo: title.bottomAnchor, constant: gridMargin).isActive = true
+        gridView.bottomAnchor.constraint(equalTo: visualEffectView.bottomAnchor, constant: -gridMargin).isActive = true
+        
+        gridView.columnSpacing = gridMargin / 2
+        gridView.rowSpacing = gridMargin / 2
+        
+        for i in 0 ..< gridSize {
+            gridView.row(at: i).height = 64
+            gridView.column(at: i).width = 64
+        }
     }
 }
 
